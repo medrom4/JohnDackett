e82dda5cd8815f479ef5eb156a6bda558f568f7a
Ramanovich <medikroma@gmail.com>
2018-09-30 19:02:51 +0000

    sort-numbers

--- js/script.js
+++ js/script.js
@@ -1,35 +1,6 @@
-(function() {
+var prices = [1, 2, 3, 54, 123, 756, 43, 98, 33, 21, 32, 55, 76];
 
-    var $imgs = $('#gallery img');
-    var $search = $('#filter-search');
-    var cache = [];
-
-    $imgs.each(function() {
-        cache.push({
-            element: this,
-            text: this.alt.trim().toLowerCase()
-        });
-    });
-
-
-    function filter() {
-        var query = this.value.trim().toLowerCase();
-
-        cache.forEach(function(img) {
-            var index = 0;
-            if (query) {
-                index = img.text.indexOf(query);
-            }
-
-            img.element.style.display = index === -1 ? 'none' : '';
-        });
-    }
-
-    if ('oninput' in $search[0]) {
-        $search.on('input', filter);
-    }
-    else {
-        $search.on('keyup', filter);
-    }
-
-}());
+prices.sort(function(a, b) {
+    return a - b;
+});
+console.log(prices);
