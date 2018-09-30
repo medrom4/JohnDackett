226fa63e8d0675b6aa1cd275d3a021b848a45665
Ramanovich <medikroma@gmail.com>
2018-09-30 18:38:38 +0000

    filter-search

--- js/script.js
+++ js/script.js
@@ -1,41 +1,35 @@
 (function() {
 
     var $imgs = $('#gallery img');
-    var $buttons = $('#buttons');
-    var tagged = {};
+    var $search = $('#filter-search');
+    var cache = [];
 
     $imgs.each(function() {
-        var img = this;
-        var tags = $(this).data('tags');
-
-        if (tags) {
-            tags.split(',').forEach(function(tagName) {
-                if (tagged[tagName] == null) {
-                    tagged[tagName] = [];
-                }
-                tagged[tagName].push(img);
-            });
-        }
+        cache.push({
+            element: this,
+            text: this.alt.trim().toLowerCase()
+        });
     });
 
 
-    $('<button/>', {
-        text: 'Все',
-        class: 'active',
-        click: function() {
-            $(this).addClass('active').siblings().removeClass('active');
-            $imgs.show(100);
-        }
-    }).appendTo($buttons);
+    function filter() {
+        var query = this.value.trim().toLowerCase();
 
-    $.each(tagged, function(tagName) {
-        $('<button/>', {
-            text: tagName + '(' + tagged[tagName].length + ')',
-            click: function() {
-                $(this).addClass('active').siblings().removeClass('active');
-                $imgs.hide().filter(tagged[tagName]).show(100);
+        cache.forEach(function(img) {
+            var index = 0;
+            if (query) {
+                index = img.text.indexOf(query);
             }
-        }).appendTo($buttons);
-    });
+
+            img.element.style.display = index === -1 ? 'none' : '';
+        });
+    }
+
+    if ('oninput' in $search[0]) {
+        $search.on('input', filter);
+    }
+    else {
+        $search.on('keyup', filter);
+    }
 
 }());
