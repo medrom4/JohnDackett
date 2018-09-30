071e222660883ca494a5cac48fe9b769aca0211a
Ramanovich <medikroma@gmail.com>
2018-09-30 19:09:42 +0000

    sort-numbers-mass

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-var prices = [1, 2, 3, 54, 123, 756, 43, 98, 33, 21, 32, 55, 76];
-
-prices.sort(function(a, b) {
-    return 0.5 - Math.random();
+var items = [
+    { name: 'Edward', value: 21 },
+    { name: 'Sharpe', value: 37 },
+    { name: 'And', value: 45 },
+    { name: 'The', value: -12 },
+    { name: 'Magnetic' },
+    { name: 'Zeros', value: 37 }
+];
+items.sort(function(a, b) {
+    if (a.value > b.value) {
+        return 1;
+    }
+    if (a.value < b.value) {
+        return -1;
+    }
+    // a должно быть равным b
+    return 0;
 });
-console.log(prices);
+
+console.log(items);
