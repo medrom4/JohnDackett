cede2ec336ea55c642ccee1e51c3b4be468fd640
Ramanovich <medikroma@gmail.com>
2018-10-05 20:46:00 +0000

    textarea-counter_v2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 (function() {
     var bio = document.getElementById('bio');
     var bioCount = document.getElementById('bio-count');
+
+    addEvent(bio, 'focus', updateCounter);
+    addEvent(bio, 'input', updateCounter);
+    addEvent(bio, 'blur', function() {
+        if (bio.value.length <= 140) {
+            bioCount.className = 'hide';
+        }
+    });
+    
+    
 }());
