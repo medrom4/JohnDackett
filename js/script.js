883db8a060e7815dacbf82460f07f1814306a1e2
Ramanovich <medikroma@gmail.com>
2018-10-05 20:27:37 +0000

    populate-selectbox_v3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 (function() {
     var type = document.getElementById('equipmentType');
     var model = document.getElementById('model');
+
     var cameras = {
         bolex: 'Bolex Paillard H8',
         yashica: 'Yashica 30',
@@ -13,4 +14,29 @@
         eumig: 'Eumig Mark S',
         sankyo: 'Sankyo Dualux'
     };
+
+    addEvent(type, 'change', function() {
+        if (this.value === 'choose') {
+            model.innerHTML = '<option>Please choose a type first</option>';
+            return;
+        }
+
+        var models = getModels(this.value);
+
+        var options = '<option>Please choose a model</option>';
+        for (var key in models) {
+            options += '<option value="' + key + '">' + models[key] + '</option>';
+        }
+
+        model.innerHTML = options;
+    });
+
+    function getModels(equipmentType) {
+        if (equipmentType === 'cameras') {
+            return cameras;
+        }
+        else if (equipmentType === 'projectors') {
+            return projectors;
+        }
+    }
 }());
